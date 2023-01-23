cd260c004411e2e03759b94aea33c6115c51c998
Anthony cardenas <logicainformatica18@gmail.com>
2023-01-22 19:31:47 -0500

    modificando codigo de pais

--- function.js
+++ function.js
@@ -1,9 +1,9 @@
 
 function send(){
-
+    var codigo_pais =w.codigo_pais.value
     var numero_destino=w.numero_destino.value;
     var message=w.message.value;
-    let url = "https://api.whatsapp.com/send?phone=51"+ numero_destino+"&text=*"+ message + " *%0A";
+    let url = "https://api.whatsapp.com/send?phone="+codigo_pais+ numero_destino+"&text=*"+ message + " *%0A";
     window.open(url);
 }
 
